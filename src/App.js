2151309600c4baf2f864494d62f0813b09611fbf
hr@redbench.us <megan@ensarsolutions.com>
2023-03-16 18:38:39 +0530

    A04

--- src/App.js
+++ src/App.js
@@ -3,26 +3,74 @@ import Register from "./Pages/register/Register";
 //import "./App.css"
 import {
   createBrowserRouter,
+  Navigate,
+  Outlet,
   RouterProvider,
-  Route,
+  //Route,
 } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./Pages/home/Home";
+import Profile from "./Pages/profile/Profile";
+//import { Children } from "react";
 
 function App() {
 
+  const currentUser = true;
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    )
+  }
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+    return children
+  }
+
   const router = createBrowserRouter([
     {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/home",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ]
+    },
+    {
       path: "/login",
-      element: <Login/>,
+      element: <Login />,
     },
     {
       path: "/register",
-      element: <Register/>,
+      element: <Register />,
     },
   ]);
 
   return (
     <div>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </div>
   );
 }
