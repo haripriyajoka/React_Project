ce557b1426d9aa71a0d7866f24bbef9f005644ed
hr@redbench.us <megan@ensarsolutions.com>
2023-03-17 17:59:28 +0530

    A05

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,39 @@
 import "./navbar.scss"
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>LayaSocial</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search...." />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src="" alt="" />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
